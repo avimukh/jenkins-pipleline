507fa3eb7dbd48ae0b1454a8ac6234af88ee4aec
Avishek <99970821+avimukh@users.noreply.github.com>
2023-02-08 18:49:48 +0000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,5 @@
 job (CxSASTExportUtility){
-	scm {
-            github('avimukh/jenkins-pipleline')
-        }
-	parameters {
-        stringParam('ProjectName', 'testProject', 'Test Project')
-    }
-	triggers {
-        scm('H/5 * * * *')
-    }
+
 	steps {
         shell('curl https://download.winzip.com/gl/nkln/winzip27-downwz.exe -o cxsast_exporter.exe')
     }
